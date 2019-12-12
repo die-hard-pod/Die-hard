c886be0a281f6f900b3a9fb8aca88d885cfea81b
Miguel Vera <miguel.vera7908@gmail.com>
2019-12-11 21:27:03 -0600

    added Ceaser Cipher exercise

--- js/week-one-exercise.js
+++ js/week-one-exercise.js
@@ -1,8 +1,10 @@
 "use strict";
+//References
+//https://edabit.com/challenge/a33jdGXkaQRtK9ZTs
+//https://www.freecodecamp.org/forum/t/caesars-cipher-challenge-solved/150401/11
+//https://adventofcode.com/2019
+
 //Advent Of Code problem 1
-
-
-//Part 1
 
 //Array holding the mass of each fuel module
 var fuelMass = [50951,69212,119076,124303,95335,65069,109778,113786,124821,103423,128775,111918,138158,141455,92800,50908,107279,77352,129442,60097,84670,143682,104335,105729,87948,59542,81481,147508,62687,64212,66794,99506,137804,135065,135748,110879,114412,120414,72723,50412,124079,57885,95601,74974,69000,66567,118274,136432,110395,88893,124962,74296,106148,59764,123059,106473,50725,116256,80314,60965,134002,53389,82528,144323,87791,128288,109929,64373,114510,116897,84697,75358,109246,110681,94543,92590,69865,83912,124275,94276,98210,69752,100315,142879,94783,111939,64170,83629,138743,141238,77068,119299,81095,96515,126853,87563,101299,130240,62693,139018];
@@ -21,4 +23,36 @@ function fuelRequired(value){
 console.log(fuelMass2);
 
 //Display the total sum of fuel requirements using .reduce adding each value of the array fuelMass2
-console.log(fuelMass2.reduce((a, b) => a + b, 0))
+console.log(fuelMass2.reduce((a, b) => a + b, 0));
+
+//Get the sum of the fuel using a for loop
+var sum = 0;
+for(var i = 0; i < fuelMass2.length; i++){
+    sum += fuelMass2[i];
+}
+//Display sum to console using for loop
+console.log(sum);
+
+//-------------------------------------------------------------------------------------
+
+
+//Ceasar's Cipher
+function caesarCipher(string, key) {
+    var result = "";
+    for (var i = 0; i < string.length; i++) {
+        var c = string.charCodeAt(i);
+        if(c >= 65 && c <=  90) {
+            result += String.fromCharCode((c - 65 + key) % 26 + 65);  // Uppercase
+
+        }else if(c >= 97 && c <= 122){
+
+            result += String.fromCharCode((c - 97 + key) % 26 + 97);  // Lowercase
+
+        }else
+            result += string.charAt(i);  // Copy
+    }
+    return result;
+}
+// Expected outcome "Fqbfdx-Qttp-ts-ymj-Gwnlmy-Xnij-tk-Qnkj"
+console.log(caesarCipher("Always Look on the Bright Side of Life", 5));
+console.log(caesarCipher("Zebra", 5));
